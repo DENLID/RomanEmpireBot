66b9b2c7371881d2419117fd0007a3dc597b4601
DENLID <127737950+DENLID@users.noreply.github.com>
2024-03-22 11:12:20 +0200

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-token = "MTIxOTcwNTEzMTYyMTI4NTg4OQ.GRBLl0.JB8mSsi5AOy7whzD6eeViHJ_0qGV8yv22Y2Ccc"
-
-mongo_api = "mongodb+srv://denlid:674527@cluster0.n2wppcr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
